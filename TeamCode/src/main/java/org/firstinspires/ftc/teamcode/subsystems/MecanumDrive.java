371d8133cc6640ca04fa02811fb3c6f344671060
aabanakhtar-github <aabanakhtar.home@gmail.com>
2024-09-27 22:58:00 -0400

    Added Mecanum Test (For testing)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/MecanumDrive.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class MecanumDrive {
+    public DcMotorEx frontLeft, frontRight, backLeft, backRight;
+
+    public MecanumDrive(HardwareMap map) {
+       frontLeft = map.get(DcMotorEx.class, "fl");
+       frontRight = map.get(DcMotorEx.class, "fr");
+       backLeft = map.get(DcMotorEx.class, "bl");
+       backRight = map.get(DcMotorEx.class, "br");
+    }
+
+
+    public void setPowers(float x, float y, float rx) {
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (y + x + rx) / denominator;
+        double backLeftPower = (y - x + rx) / denominator;
+        double frontRightPower = (y - x - rx) / denominator;
+        double backRightPower = (y + x - rx) / denominator;
+
+        frontLeft.setPower(frontLeftPower);
+        backLeft.setPower(backLeftPower);
+        frontRight.setPower(frontRightPower);
+        backRight.setPower(backRightPower);
+    }
+}
