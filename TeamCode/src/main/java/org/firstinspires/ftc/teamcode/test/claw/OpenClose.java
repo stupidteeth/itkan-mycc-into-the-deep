65e10c7a47cf806582382feb79509df680c253b4
Tala AHmed <talalaahmed2008@gmail.com>
2024-11-02 14:16:50 -0400

    claw try

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/claw/OpenClose.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.test.claw;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class OpenClose extends OpMode {
+    private Servo openClose;
+    double position;
+    boolean button = gamepad1.a;
+
+    @Override
+    public void init() {
+        openClose = hardwareMap.get(Servo.class, "openClose");
+
+        public void setServoPosition(double position); {
+            openClose.setPosition(position);
+
+        }
+    }
+
+    @Override
+    public void loop() {
+
+        while (position >= 0){
+            gamepad1.a button = true;
+        }
+
+
+        while (gamepad1.a = true) {
+
+
+        }
+
+        }
+
+}
